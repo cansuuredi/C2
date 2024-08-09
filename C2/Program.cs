a680595478cdb3eef461a146bc528ad578e336fb
BADAM <BADAM@DESKTOP-QPCL77O>
2024-08-09 16:37:07 +0300

    Add project files.

---
+++ C2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//2. Dereceden denklemlerin koklerini inceleyen kodu yazınız
+//1.ihtiyaca göre değişkenleri tanımla
+int a, b, c, delta;
+//2. katsayıları kullanıcıdan al
+Console.Write("a katsayisini gir");
+a = Convert.ToInt32((Console.ReadLine()));
+Console.Write("b katsayisini gir");
+b = Convert.ToInt32((Console.ReadLine()));
+Console.Write("c katsayisini gir");
+c = Convert.ToInt32((Console.ReadLine()));
+//3. deltayı hesapla
+delta = b * b - (4 * a * c);
+
+//4. deltaya göre kök durumunu incele
+
+if (delta > 0)
+    Console.WriteLine("Reel iki kök var");
+else if (delta == 0)
+    Console.WriteLine("Tek kök var");
+else
+    Console.WriteLine("Reel kök yok");
---
+++ Toplama/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Aşağıdaki Toplama İşlemini Yapınız.");
+
+//randomu başa çekersen eğer sürekli ilk sorduğu soruya döner. 
+//randomu for'un içine alırsan sürekli farklı bir işlem üretir. 
+//break ilk "if"ten sonra çünkü döngünün orada kırılması lazım. 
+//for'un baştaki hali sonsuz döngü oluşturur.
+for (; ; )
+{
+    Random rnd = new Random();
+    int sayi1 = rnd.Next(1, 101);
+    int sayi2 = rnd.Next(1, 101);
+    Console.WriteLine("{0} + {1} = ?", sayi1, sayi2);
+    Console.WriteLine("Cevabınız Nedir?");
+    int sonuc = int.Parse(Console.ReadLine());
+
+    if (sonuc == (sayi1 + sayi2))
+    {
+        Console.WriteLine("Tebrikler");
+        
+        break;
+    }
+    else {
+        Console.WriteLine("Tekrar dene!");
+    }
+        
+    }
